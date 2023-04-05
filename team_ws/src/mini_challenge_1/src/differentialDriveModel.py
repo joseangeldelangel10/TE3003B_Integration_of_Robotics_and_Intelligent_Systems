742f2cc0f334ddcc82db40982cb9e354fca0e107
JOSE <joseangeldelangel10@gmail.com>
2023-04-05 00:35:40 -0600

    v1 of our puzzlebot sim completed, tests succeded

--- team_ws/src/mini_challenge_1/src/differentialDriveModel.py
+++ team_ws/src/mini_challenge_1/src/differentialDriveModel.py
@@ -3,6 +3,7 @@
 import rospy
 import numpy as np
 from geometry_msgs.msg import Twist, Pose2D
+
 class DifferentialDriveModel:
     
     def __init__(self):
@@ -14,6 +15,7 @@ class DifferentialDriveModel:
         self.puzzlbeot_pose_th = None
         self.twist_linear_x = None
         self.twist_angular_z = None
+        self.rate = rospy.Rate(10.0)
 
     def pose_callback(self, msg):
         self.puzzlbeot_pose_th = msg.theta
@@ -23,11 +25,13 @@ class DifferentialDriveModel:
         self.twist_angular_z = msg.angular.z        
 
     def main(self):
-        if self.puzzlbeot_pose_th is not None and self.twist_linear_x is not None and self.twist_angular_z is not None:
-            self.puzzlebot_twist_on_base_frame.linear.x = self.twist_linear_x*np.cos(self.puzzlbeot_pose_th)
-            self.puzzlebot_twist_on_base_frame.linear.y = self.twist_linear_x*np.sin(self.puzzlbeot_pose_th)
-            self.puzzlebot_twist_on_base_frame.angular.z = self.twist_angular_z
-            self.puzzlebot_twist_on_base_frame_pub.publish(self.puzzlebot_twist_on_base_frame)
+        while not rospy.is_shutdown():
+            if self.puzzlbeot_pose_th is not None and self.twist_linear_x is not None and self.twist_angular_z is not None:
+                self.puzzlebot_twist_on_base_frame.linear.x = self.twist_linear_x*np.cos(self.puzzlbeot_pose_th)
+                self.puzzlebot_twist_on_base_frame.linear.y = self.twist_linear_x*np.sin(self.puzzlbeot_pose_th)
+                self.puzzlebot_twist_on_base_frame.angular.z = self.twist_angular_z
+                self.puzzlebot_twist_on_base_frame_pub.publish(self.puzzlebot_twist_on_base_frame)
+            self.rate.sleep()
 
 if __name__ == '__main__':
     ddm = DifferentialDriveModel()
---
+++ team_ws/src/mini_challenge_1/src/solver.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+from geometry_msgs.msg import Twist, Pose2D
+
+class Solver:
+    
+    def __init__(self, initial_x = 0.0, initial_y = 0.0, initial_theta = 0.0):
+        rospy.init_node('solver')
+        self.twist_sub = rospy.Subscriber('/puzzlebot_vel_on_base_frame_pub', Twist, self.twist_callback)
+        self.puzzlebot_pose_pub = rospy.Publisher('/pose', Pose2D, queue_size=1)
+        self.puzzlebot_pose = Pose2D()
+        self.puzzlebot_pose.x, self.puzzlebot_pose.y, self.puzzlebot_pose.theta = (initial_x, initial_y, initial_theta)
+        self.puzzlebot_vel_on_base_frame_x = None
+        self.puzzlebot_vel_on_base_frame_y = None
+        self.puzzlebot_vel_on_base_frame_th = None
+        self.first_time_with_twist = True
+        self.last_sampling_time = None
+        self.rate = rospy.Rate(10.0)    
+
+    def twist_callback(self, msg):
+        self.puzzlebot_vel_on_base_frame_x = msg.linear.x
+        self.puzzlebot_vel_on_base_frame_y = msg.linear.y
+        self.puzzlebot_vel_on_base_frame_th = msg.angular.z
+
+    def main(self):
+        while not rospy.is_shutdown():
+            if self.puzzlebot_vel_on_base_frame_x is not None and self.puzzlebot_vel_on_base_frame_y is not None and self.puzzlebot_vel_on_base_frame_th is not None:
+                if self.first_time_with_twist:
+                    self.first_time_with_twist = False
+                    self.last_sampling_time = rospy.get_time()
+                    print("fist time vel passed")
+                else:
+                    current_time = rospy.get_time()
+                    delta_t = (current_time - self.last_sampling_time)
+                    self.puzzlebot_pose.x += self.puzzlebot_vel_on_base_frame_x*delta_t
+                    self.puzzlebot_pose.y += self.puzzlebot_vel_on_base_frame_y*delta_t
+                    self.puzzlebot_pose.theta += self.puzzlebot_vel_on_base_frame_th*delta_t
+                    self.last_sampling_time = current_time
+            self.puzzlebot_pose_pub.publish(self.puzzlebot_pose)
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    instance = Solver()
+    instance.main()
