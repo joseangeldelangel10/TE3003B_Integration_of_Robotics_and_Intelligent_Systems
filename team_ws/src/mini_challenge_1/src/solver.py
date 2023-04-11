4b0ec8493c1d60046103e42244198c6ec02d2026
JOSE <joseangeldelangel10@gmail.com>
2023-04-11 01:02:07 -0600

    reset sim service added to our simulation

--- team_ws/src/mini_challenge_1/src/solver.py
+++ team_ws/src/mini_challenge_1/src/solver.py
@@ -3,14 +3,16 @@
 import rospy
 import numpy as np
 from geometry_msgs.msg import Twist, Pose2D
+from mini_challenge_1.srv import *
 
 class Solver:
     
     def __init__(self, initial_x = 0.0, initial_y = 0.0, initial_theta = 0.0):
         rospy.init_node('solver')
         self.twist_sub = rospy.Subscriber('/puzzlebot_vel_on_base_frame_pub', Twist, self.twist_callback)
-        self.puzzlebot_pose_pub = rospy.Publisher('/pose', Pose2D, queue_size=1)
+        self.puzzlebot_pose_pub = rospy.Publisher('/pose', Pose2D, queue_size=1)        
         self.puzzlebot_pose = Pose2D()
+        self.reset_puzzlebot_service_result = rospy.Service("reset_puzzlebot_sim", ResetPuzzlebotSim, self.reset_puzzlebot_sim)
         self.puzzlebot_pose.x, self.puzzlebot_pose.y, self.puzzlebot_pose.theta = (initial_x, initial_y, initial_theta)
         self.puzzlebot_vel_on_base_frame_x = None
         self.puzzlebot_vel_on_base_frame_y = None
@@ -18,6 +20,12 @@ class Solver:
         self.first_time_with_twist = True
         self.last_sampling_time = None
         self.rate = rospy.Rate(10.0)    
+
+    def reset_puzzlebot_sim(self, req):
+        self.puzzlebot_pose.x = 0.0
+        self.puzzlebot_pose.y = 0.0
+        self.puzzlebot_pose.theta = 0.0
+        return ResetPuzzlebotSimResponse(True)
 
     def twist_callback(self, msg):
         self.puzzlebot_vel_on_base_frame_x = msg.linear.x
