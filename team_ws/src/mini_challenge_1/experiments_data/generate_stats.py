780e7fa546a1423398fcac44857ec7b90ec6e626
LeoNava2202 <A01750595@tes.mx>
2023-04-11 21:01:20 -0600

    updating stats script

--- team_ws/src/mini_challenge_1/experiments_data/generate_stats.py
+++ team_ws/src/mini_challenge_1/experiments_data/generate_stats.py
@@ -8,21 +8,58 @@ df = pd.read_csv(name_csv)
 
 data = df.values
 
-xf = data[:,4]
-yf = data[:,6]
+xf = [[],[],[]]
+yf = [[],[],[]]
+thf = [[],[],[]]
 
+print(data[:,3])
+
+
+for i in range(len(data)):
+    if data[i,5] == 1:
+        xf[0].append(data[i,4])
+        yf[0].append(data[i,6])
+    elif data[i,5] == 2:
+        xf[1].append(data[i,4])
+        yf[1].append(data[i,6])
+    elif data[i,5] == 3:
+        xf[2].append(data[i,4])
+        yf[2].append(data[i,6])
+    elif abs(data[i,3] - 1.57079633) < 0.01:
+        thf[0].append(data[i,2])
+
+print("X")
+print(xf)
+print("")
+print("Y")
+print(yf)
+print("")
+print("Theta")
+print(thf)
 
 plt.figure()
-plt.scatter(xf, yf, marker="*")
+plt.scatter(xf[0], yf[0], marker="*", c="blue")
+plt.scatter(xf[1], yf[1], marker="*", c="red")
+plt.scatter(xf[2], yf[2], marker="*",c ="green")
 plt.title('Dispersion')
 
 plt.figure()
-plt.hist(xf)
-plt.title('Distribucion en x')
+plt.hist(xf[0])
+plt.hist(xf[1])
+plt.hist(xf[2])
+plt.title('Histograma en x')
 
 plt.figure()
-plt.hist(yf)
-plt.title('Distribucion en y')
+plt.hist(yf[0])
+plt.hist(yf[1])
+plt.hist(yf[2])
+plt.title('Histograma en y')
+
+plt.figure()
+plt.hist(thf[0])
+#plt.hist(thf[1])
+#plt.hist(thf[2])
+plt.title('Histograma en theta')
 
 
 plt.show()
