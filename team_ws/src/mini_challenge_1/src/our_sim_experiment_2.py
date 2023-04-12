52befe20d790ab3cf22e119c6e69b5372040a7e6
JOSE <joseangeldelangel10@gmail.com>
2023-04-11 19:22:45 -0600

    our sim exp1 completed

--- team_ws/src/mini_challenge_1/src/our_sim_experiment_2.py
+++ team_ws/src/mini_challenge_1/src/our_sim_experiment_2.py
@@ -6,6 +6,7 @@ import os
 from point2PointController import Point2PointController
 from geometry_msgs.msg import Twist, Pose2D, Pose
 from nav_msgs.msg import Odometry
+from mini_challenge_1.srv import *
 
 class PuzzlebotP2PExperiment():
     def __init__(self):        
@@ -13,7 +14,16 @@ class PuzzlebotP2PExperiment():
         self.experiment_pose_sub = None
         self.csv_file_name = None
         self.data_saved = False
-        self.rate = rospy.Rate(20.0)        
+        self.rate = rospy.Rate(20.0)                
+
+    def reset_puzzlebot_sim(self):
+        rospy.wait_for_service('reset_puzzlebot_sim')
+        try:
+            reset_sim = rospy.ServiceProxy('reset_puzzlebot_sim', ResetPuzzlebotSim)
+            response = reset_sim()
+            return response.success
+        except rospy.ServiceException:
+            print("reset sim service call failed")
 
     def define_nav_pose_topic(self, topic_name, topic_d_type):
         self.p2p_contr.define_nav_pose_topic(topic_name, topic_d_type)
@@ -44,7 +54,10 @@ class PuzzlebotP2PExperiment():
 
     def save_resulting_goal(self, csv_file_name, resulting_pose_2d):
         df = pd.read_csv(csv_file_name)
-        last_run_number = max(df["run_num"])
+        try:
+            last_run_number = max(df["run_num"])
+        except ValueError:
+            last_run_number = 0
         new_data = {"run_num": [last_run_number + 1], 
                     "t_f":[None],
                     "x_goal":[self.p2p_contr.goal[0]], 
@@ -61,12 +74,19 @@ class PuzzlebotP2PExperiment():
     def main(self):
         while not rospy.is_shutdown():
             self.p2p_contr.inherited_main()                
-            self.rate.sleep()        
+            self.rate.sleep()
+            if self.data_saved:
+                break        
         
 
-if __name__ == '__main__':
-    p2p_experiment = PuzzlebotP2PExperiment()
-    p2p_experiment.define_nav_pose_topic("/pose", Pose2D)
-    p2p_experiment.define_experiment_pose_topic("/pose", Pose2D)
-    p2p_experiment.define_csv_file_to_save_experiment_data("our_sim.csv")
-    p2p_experiment.main()
+if __name__ == '__main__':    
+    total_experiments = 10
+    completed_experiments = 0
+    while (total_experiments - completed_experiments) > 0:     
+        p2p_experiment = PuzzlebotP2PExperiment()
+        p2p_experiment.define_nav_pose_topic("/pose", Pose2D)
+        p2p_experiment.define_experiment_pose_topic("/pose", Pose2D)
+        p2p_experiment.define_csv_file_to_save_experiment_data("our_sim.csv")
+        p2p_experiment.main()
+        p2p_experiment.reset_puzzlebot_sim()
+        completed_experiments += 1
