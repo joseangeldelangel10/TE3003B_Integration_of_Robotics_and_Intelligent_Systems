191d42b497a5afeb741086aeb987eac522fa5898
JOSE <joseangeldelangel10@gmail.com>
2023-04-14 18:09:15 -0600

    executed sim open loop experiments

--- team_ws/src/mini_challenge_1/src/our_sim_experiment_3.py
+++ team_ws/src/mini_challenge_1/src/our_sim_experiment_3.py
@@ -13,7 +13,7 @@ class PuzzlebotOpenLoopExperiment():
     def __init__(self):        
         rospy.init_node('puzzlebot_experiment')
         #self.p2p_contr = Point2PointController()
-        self.linear_vel, self.angular_vel, self.exec_time = (0.2, 0.0, 1.0)
+        self.linear_vel, self.angular_vel, self.exec_time = (0.0, np.pi/5, 3.0)
         self.cmd_vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=1)
         self.cmd_vel_msg = Twist()
         self.experiment_pose_sub = None
@@ -47,7 +47,7 @@ class PuzzlebotOpenLoopExperiment():
         self.experiment_pose_sub = rospy.Subscriber(pose_topic, topic_d_type, self.experiment_pose_callback)
 
     def experiment_pose_callback(self, msg):
-        if (rospy.get_time() - self.inital_time) >= self.exec_time and not self.data_saved:
+        if self.inital_time is not None and (rospy.get_time() - self.inital_time) >= self.exec_time and not self.data_saved:
             if self.csv_file_name is not None:
                 if str(type(msg)) == "<class 'geometry_msgs.msg._Pose2D.Pose2D'>":            
                     self.save_resulting_goal(self.csv_file_name, (msg.x, msg.y, msg.theta))
@@ -76,7 +76,7 @@ class PuzzlebotOpenLoopExperiment():
                     "y_f":[resulting_pose_2d[1]], 
                     "th_f":[resulting_pose_2d[2]],
                     "linear_vel": [self.linear_vel], 
-                    "angular_vel":[self.angular_vel]}        
+                    "ang_vel":[self.angular_vel]}        
         df2 = pd.DataFrame(new_data)
         df = df.append(df2, sort=True)
         df.to_csv(csv_file_name, index=False)        
