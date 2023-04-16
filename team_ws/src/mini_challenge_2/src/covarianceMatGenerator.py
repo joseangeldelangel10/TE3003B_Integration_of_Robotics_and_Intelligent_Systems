14f0b9c184a42b6d5ae8d7426a4be2de0f3180e3
JOSE <joseangeldelangel10@gmail.com>
2023-04-15 20:13:53 -0600

    added get puzzlebot covariance mat service

--- team_ws/src/mini_challenge_2/src/covarianceMatGenerator.py
+++ team_ws/src/mini_challenge_2/src/covarianceMatGenerator.py
@@ -4,12 +4,25 @@ import pandas as pd
 import rospkg
 import os
 import numpy as np
+from std_msgs.msg import Float64MultiArray, MultiArrayDimension
+from mini_challenge_2.srv import *
 
 class CovarianceMatrixGenerator():
     def __init__(self):      
         rospy.init_node('covariance_mat_generator')  
         self.csv_file_name = self.define_csv_file_with_experiment_data("dummy_experiments_for_testing/physical.csv")    
-        self.matrix_generated = False
+        self.get_covariance_mat_server = rospy.Service("get_puzzlebot_covariance_mat", getPuzzlebotCovarianceMat, self.get_covariance_mat)
+        self.get_covariance_mat_result = Float64MultiArray()
+        self.covariance_mat_height_layout = MultiArrayDimension()
+        self.covariance_mat_width_layout = MultiArrayDimension()
+        
+        self.covariance_mat_height_layout.label = "height"
+        self.covariance_mat_height_layout.size = 3
+        self.covariance_mat_height_layout.stride = 3*3
+        self.covariance_mat_width_layout.label = "width"
+        self.covariance_mat_width_layout.size = 3
+        self.covariance_mat_width_layout.stride = 3
+        
         self.rate = rospy.Rate(20.0)                
 
     def define_csv_file_with_experiment_data(self, csv_file_name):
@@ -17,33 +30,40 @@ class CovarianceMatrixGenerator():
         package_path = rp.get_path('mini_challenge_1')
         final_csv_file_name = os.path.join(package_path, "experiments_data", csv_file_name)
         return final_csv_file_name
-
-    def get_covariance_mat(self):
-        df = pd.read_csv(self.csv_file_name)
-        df = df[df["x_f"] > 0.5]
-        df = df[df["x_f"] < 1.5]
-        df = df[["x_f", "y_f", "th_f"]]
-        covariance_mat = np.cov(df.to_numpy() , rowvar=False)
-        
-        df_only_x = df["x_f"].to_numpy()
+    
+    def get_single_variable_variance(self, df, variable):
+        df_only_x = df[variable].to_numpy()
         n_samples = len(df_only_x)
         x_mean = df_only_x.mean()
         df_only_x = df_only_x.reshape((n_samples, 1)) - x_mean
-        x_variance = np.dot(df_only_x.T, df_only_x)/(n_samples-1)
-        #df2 = pd.DataFrame(new_data)
-        #df = df.append(df2, sort=True)        
-        print("data frame \n {d}".format(d = df))
-        print("covariance mat \n {m}".format(m = covariance_mat))
-        print("x var \n {x}".format(x = x_variance))
-        self.matrix_generated = True
+        x_variance = np.dot(df_only_x.T, df_only_x)/(n_samples-1)        
+        return x_variance
+
+    def get_covariance_mat(self, req):
+        if req.experiments_file == "":
+            df = pd.read_csv(self.csv_file_name)
+        else:
+            df = pd.read_csv(req.experiments_file)                
+        df = df[df["t_f"] == 6.0]
+        df = df[df["th_f"] <= 0.7]        
+        df = df[["x_f", "y_f", "th_f"]]
+        covariance_mat = np.cov(df.to_numpy() , rowvar=False)
+                
+        rospy.loginfo("COVARIANCE MATRIX REQUESTED")
+        rospy.loginfo("respose prove: \t x_variance_in_matrix={v1} \t x_variance_manual={v2}".format(
+            v1 = covariance_mat[0][0],
+            v2 = self.get_single_variable_variance(df, "x_f")
+        ))        
+
+        cov_mat_n, cov_mat_m = covariance_mat.shape
+        self.get_covariance_mat_result.layout.dim = [self.covariance_mat_height_layout, self.covariance_mat_width_layout]        
+        self.get_covariance_mat_result.layout.data_offset = 0
+        self.get_covariance_mat_result.data = covariance_mat.reshape((cov_mat_m*cov_mat_n,)) 
+        return getPuzzlebotCovarianceMatResponse(self.get_covariance_mat_result)
 
     def main(self):
         while not rospy.is_shutdown():
-            self.get_covariance_mat()
-            self.rate.sleep()
-            if self.matrix_generated:
-                break                    
-        
+            rospy.spin()                            
 
 if __name__ == '__main__':    
     cov_mat_generator = CovarianceMatrixGenerator()
