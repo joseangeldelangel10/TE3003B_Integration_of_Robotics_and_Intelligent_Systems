2528224ab3894d1a1680b179fe44756e94981310
JOSE <joseangeldelangel10@gmail.com>
2023-04-17 15:53:06 -0600

    added q_k calculation v1 - still needs review since q_k is not varying

--- team_ws/src/mini_challenge_2/src/wheelBasedDeadReckoning.py
+++ team_ws/src/mini_challenge_2/src/wheelBasedDeadReckoning.py
@@ -2,31 +2,58 @@
 
 import rospy
 import numpy as np
+from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist, Pose2D
 from nav_msgs.msg import Odometry
 from tf.transformations import quaternion_from_euler
 from mini_challenge_1.srv import *
+import rospkg
+import os
+import json
 
 class OdometryNode():
     # TODO fix this node so that it provides puzzlebot odometry using wheel speed based dead reckoning and
     # not Twist based dead reckoning
     
     def __init__(self, initial_x = 0.0, initial_y = 0.0, initial_theta = 0.0):
-        rospy.init_node('odometry')
-        self.initial_x, self.initial_y, self.initial_theta = (initial_x, initial_y, initial_theta)        
-        self.twist_sub = rospy.Subscriber('/cmd_vel', Twist, self.twist_callback)        
+        
+        # ______________ init node publishers, subscribers and services ______________
+        rospy.init_node('odometry')        
+        self.twist_sub = rospy.Subscriber('/cmd_vel', Twist, self.twist_callback)
+        self.wr_sub = rospy.Subscriber('/wr', Float32, self.puzzlebot_wr_callback)
+        self.wl_sub = rospy.Subscriber('/wl', Float32, self.puzzlebot_wl_callback)        
         self.puzzlebot_odom_pub = rospy.Publisher('/odom', Odometry, queue_size=1)        
         self.reset_puzzlebot_service_result = rospy.Service("reset_odometry", ResetOdometry, self.reset_odometry)
+        
+        # ______________ fill in publisher messages ______________
+        self.initial_x, self.initial_y, self.initial_theta = (initial_x, initial_y, initial_theta)        
         self.puzzlebot_estimated_pose = Odometry()        
-        self.fill_odometry_header()
+        self.fill_odometry_header()        
         self.puzzlebot_estimated_pose.pose.pose.position.x, self.puzzlebot_estimated_pose.pose.pose.position.y = (self.initial_x, self.initial_y)        
         self.puzzlebot_estimated_rot = self.initial_theta        
+        
+        # ______________ init nav variables ______________
         self.puzzlebot_estimated_rot_quaternion = None
         self.puzzlebot_twist = None
         self.first_time_with_twist = True
-        self.last_sampling_time = None
+        self.last_sampling_time = None        
+        self.puzzlebot_wheel_rad = 5.0 # TODO give this the right wheel rad 
+        self.puzzlebot_wheel_to_wheel_dist = 20.0 # TODO give this the right wheel to wheel_dist
+        self.puzzlebot_wr = None
+        self.puzzlebot_wl = None
+        
+        # ______________ init covariance variables ______________
         self.covariance_matrix = None
         self.puzzlebot_model_jacobian = None        
+        rp = rospkg.RosPack()
+        this_pkg_path = rp.get_path('mini_challenge_2')
+        cov_constants_file_name = os.path.join(this_pkg_path, "src", "constants", "covariance_prop_constants.json")
+        cov_constants_file = open(cov_constants_file_name)
+        cov_constants = json.load(cov_constants_file)        
+        self.covar_kr = cov_constants["k1"]
+        self.covar_kl = cov_constants["k2"]
+
+        # ______________ init rate ______________
         self.rate = rospy.Rate(20.0)            
     
     def reset_odometry(self, req):
@@ -39,14 +66,16 @@ class OdometryNode():
         self.first_time_with_twist = True
         self.last_sampling_time = None
         rospy.loginfo("ODOMETRY RESET")
+        # TODO reset covariance when odmetry is reseted
         return ResetOdometryResponse(True)
 
+    def puzzlebot_wr_callback(self, msg):
+        self.puzzlebot_wr = msg.data
+
+    def puzzlebot_wl_callback(self, msg):
+        self.puzzlebot_wl = msg.data 
+
     def twist_callback(self, msg):
-        """
-        self.puzzlebot_vel_on_base_frame_x = msg.linear.x
-        self.puzzlebot_vel_on_base_frame_y = msg.linear.y
-        self.puzzlebot_vel_on_base_frame_th = msg.angular.z
-        """
         self.puzzlebot_twist = msg
 
     def fill_odometry_header(self):
@@ -57,7 +86,7 @@ class OdometryNode():
 
     def main(self):
         while not rospy.is_shutdown():
-            if self.puzzlebot_twist is not None:
+            if self.puzzlebot_twist is not None and self.puzzlebot_wr is not None and self.puzzlebot_wl is not None:
                 if self.first_time_with_twist:
                     self.first_time_with_twist = False
                     self.last_sampling_time = rospy.get_time()
@@ -75,7 +104,16 @@ class OdometryNode():
                          [0.0, 1.0, delta_t* self.puzzlebot_twist.linear.x* np.cos(self.puzzlebot_estimated_rot) ],
                          [0.0, 0.0, 1.0]] 
                     )
-                    q_k = np.ones((3,3))*0.001 # TODO implement real q_k formula
+                    nabla_w_k = (1/2)*self.puzzlebot_wheel_rad*delta_t*np.array(
+                        [[np.cos(self.puzzlebot_estimated_rot), np.cos(self.puzzlebot_estimated_rot)],
+                         [np.sin(self.puzzlebot_estimated_rot), np.sin(self.puzzlebot_estimated_rot)],
+                         [(2/self.puzzlebot_wheel_to_wheel_dist), -(2/self.puzzlebot_wheel_to_wheel_dist)]]
+                    )
+                    sigma_delta_k = np.array(
+                        [[self.covar_kr*abs(self.puzzlebot_wr), 0.0],
+                         [0.0, self.covar_kl*abs(self.puzzlebot_wl)]]
+                    )
+                    q_k = np.matmul( np.matmul( nabla_w_k , sigma_delta_k ), nabla_w_k.T )
                     self.covariance_matrix = np.matmul( np.matmul(self.puzzlebot_model_jacobian,self.covariance_matrix), (self.puzzlebot_model_jacobian.T)) + q_k
                     self.puzzlebot_estimated_pose.pose.covariance = (
                         self.covariance_matrix[0,0:2].tolist() + [0.0]*3 + [self.covariance_matrix[0,2]] +
