0baae8e0874a5ea3a7efca5d67c40f36b04747c4
Raulmusito <raulmusito@gmail.com>
2023-05-04 20:58:34 -0600

    bug0 upgraded

--- team_ws/src/mini_challenge_3/src/bug_0_controller.py
+++ team_ws/src/mini_challenge_3/src/bug_0_controller.py
@@ -40,14 +40,14 @@ class Bug0():
         self.current_angle = None
         self.displaced_angle = 0.0
                      
-        self.angular_error_treshold = 0.3    
+        self.angular_error_treshold = 0.8  
         self.distance_error_treshold = 0.08                    
 
         self.go2point_angular_kp = 0.08
         self.go2point_linear_kp = 0.3
 
-        self.wall_kp_follow = 40.0
-        self.wall_kp_avoid = 0.3
+        self.wall_kp_follow = 30.0
+        self.wall_kp_avoid = 1
         #self.wall_kd_avoid = -0.08
         #self.previous_wall_avoid_error = None
 
@@ -83,18 +83,15 @@ class Bug0():
         self.current_position_xy_2d = ( data.pose.pose.position.x , data.pose.pose.position.y )                             
         self.current_angle = nav_functions.calculate_yaw_angle_deg( data.pose.pose.orientation )
 
-    def turn_left(self, p2p_target_angle, angle_to_rotate = np.pi/2.0):
-        
-        if self.last_turn_time == None and self.state == "turn_left":
+    def turn_left(self, angle_to_rotate):
+        if self.last_turn_time == None:
             self.last_turn_time = rospy.get_time()
         else:    
             if self.displaced_angle < angle_to_rotate:                
-                linear_x = 0.0
-                angular_z = self.w_max            
-                self.vel_msg.linear.x = linear_x
-                self.vel_msg.angular.z = angular_z                
+                self.vel_msg.angular.z = self.w_max            
                 current_time = rospy.get_time()
-                self.displaced_angle += abs(angular_z)*(current_time - self.last_turn_time)
+                self.displaced_angle += abs(self.vel_msg.angular.z)*(current_time - self.last_turn_time)
+                print (np.rad2deg(self.displaced_angle))
                 self.last_turn_time = current_time
             else:
                 self.state = "follow_wall"
@@ -103,13 +100,14 @@ class Bug0():
                 self.last_turn_time = None            
 
     def go_to_point_controller(self, angle_error, distance_error):        
-        front_scan_val = self.get_mean_laser_value_at_fov(0.0, 30.0)        
         if distance_error <= self.distance_error_treshold:                                                            
             self.state = "arrived"
         elif self.obstacle_in_front():
-                self.state = "turn_left"
+            self.angle_to_rotate = self.set_turn_angle()
+            self.state = "turn_left"
         elif abs(angle_error) > self.angular_error_treshold:                                    
             self.vel_msg.angular.z = nav_functions.saturate_signal(self.go2point_angular_kp*angle_error, self.w_max) 
+            self.vel_msg.linear.x = 0.2
         elif distance_error > self.distance_error_treshold:              
             self.vel_msg.linear.x = nav_functions.saturate_signal(self.go2point_linear_kp*distance_error, self.v_max)    
     
@@ -170,46 +168,51 @@ class Bug0():
         side_dists_to_obstacle = self.get_values_at_target(315.0, 30.0)
         return min(values_in_front) <= self.wall_distance or min(side_dists_to_obstacle) <= self.wall_distance/2.0
     
+    def check_inf(self, value):
+        if value == np.inf:
+            return 12.0
+        else:
+            return value
+        
+    def set_turn_angle(self):
+        angle_apperture = 10.0
+        right_angle = 360.0 - angle_apperture
+        dist_at_90 = self.scan.ranges[self.get_laser_index_from_angle(0.0)]
+        dist_at_80 = self.scan.ranges[self.get_laser_index_from_angle(right_angle)]
+        rad_apperture = np.deg2rad(angle_apperture)
+        izquierda = self.check_inf(dist_at_90**2 + dist_at_80**2)
+        derecha = self.check_inf(2.0*dist_at_90*dist_at_80*math.cos(rad_apperture))
+        print ("izquierda: ", izquierda, " derecha: ", derecha)
+        abajo = math.sqrt( izquierda - derecha)
+        print ("abajo: ", abajo)
+        angle = math.asin((dist_at_80*math.sin(rad_apperture))/abajo) 
+        print ("angle: ", np.rad2deg(angle))
+        return angle
+
     def right_hand_rule_controller(self, p2p_target_angle):
         
         if self.scan != None:                       
             if self.target_path_is_clear(p2p_target_angle) and (rospy.get_time() - self.follow_wall_start_time) >= 1.0:
                 self.state = "go_to_point"
             elif self.obstacle_in_front():
+                self.angle_to_rotate = self.set_turn_angle()
                 self.state = "turn_left"                
             else:
-                # TODO complete linear_x and angular_z vals                
+                print("using RHRC")
                 dist_at_0 = self.scan.ranges[self.get_laser_index_from_angle(270.0)]
                 dist_at_10 = self.scan.ranges[self.get_laser_index_from_angle(280.0)]
-                l3 = np.sqrt( (dist_at_0**2.0) + (dist_at_10**2.0) - 2.0*dist_at_0*dist_at_10*(np.cos( 0.174533 )) )
+                dist_at_90 = self.scan.ranges[self.get_laser_index_from_angle(0.0)]
+                l3 = np.sqrt( (dist_at_0**2) + (dist_at_10**2) - 2.0*dist_at_0*dist_at_10*(np.cos( 0.174533 )) )
                 y1 = np.arcsin((dist_at_10*( np.sin(0.174533) ))/l3)
                 ang_err = np.pi/2.0 - y1                            
                 dist_to_wall = min( self.get_values_at_target(270.0, 90.0) )
                 if dist_to_wall == np.inf:
                     dist_to_wall = 12.0
-                #dist_to_wall = dist_at_0
                 relative_euclidean_distance_to_wall = self.wall_distance - dist_to_wall
 
-                """
-                if abs(ang_err) < 0.3 and abs(relative_euclidean_distance_to_wall) < 0.1: # TODO consider changing this condition or stating this tresh at init
-                    angular_z = 0.0
-                else:
-                    if abs(relative_euclidean_distance_to_wall) >= 0.1:                    
-                        # start comment
-                        if self.previous_wall_avoid_error is None:
-                            wall_dist_error_derivative = 0.0
-                            self.previous_wall_avoid_error = relative_euclidean_distance_to_wall                        
-                        else:                        
-                            wall_dist_error_derivative = (relative_euclidean_distance_to_wall - self.previous_wall_avoid_error)
-                            self.previous_wall_avoid_error = relative_euclidean_distance_to_wall
-                        # end comment
-                        #angular_z = self.wall_kp_follow*ang_err
-                        angular_z = self.wall_kp_avoid*relative_euclidean_distance_to_wall
-                    elif abs(ang_err) >= 0.3:
-                        angular_z = self.wall_kp_follow*ang_err
-                """
                 angular_z = nav_functions.saturate_signal( self.wall_kp_avoid*relative_euclidean_distance_to_wall, self.w_max )
                 if self.wall_kp_follow*ang_err != 0:
+                    print("normal")
                     linear_x = nav_functions.saturate_signal( 1/(self.wall_kp_follow* abs(ang_err)), self.v_max_wall )
                     if linear_x <= self.v_max_wall/2.0:
                         linear_x = self.v_max_wall/2.0
@@ -217,8 +220,10 @@ class Bug0():
                     linear_x = self.v_max_wall
 
                 if dist_at_0 >= 1.2 or ang_err < (-np.pi/2.0)*(1.0/3.0):
-                    angular_z = -(self.v_max_wall/self.wall_distance)
-                    linear_x = self.v_max_wall                            
+                    print("girando")
+                    angular_z = -(0.2/self.wall_distance)
+                    linear_x = 0.12    
+      
 
                 self.vel_msg.angular.z = angular_z
                 self.vel_msg.linear.x = linear_x
@@ -240,7 +245,7 @@ class Bug0():
                 elif self.state == "follow_wall":
                     self.right_hand_rule_controller(target_angle)  
                 elif self.state == "turn_left":
-                    self.turn_left(target_angle) 
+                    self.turn_left(self.angle_to_rotate) 
                 elif self.state == "arrived":
                     print("ia llegue")
                     exit             
